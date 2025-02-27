80ed6f3f6b71b24cbf44fd7d1b9312d7693ceacd
Stasies <tia.nntr.wth@gmail.com>
2025-02-27 17:03:28 +0300

    add test dir

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    dir: './src/tests/',
     setupFiles: ['__setups__/canvas.ts'],
     environment: 'happy-dom', // Simulates a browser
     globals: true,
